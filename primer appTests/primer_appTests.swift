1295993dd3e15b545c573e8dffd0ff1d1b97bf3f
José Moreno <107092442+Josemor12@users.noreply.github.com>
2025-07-07 22:11:09 -0500

    Add files via upload

---
+++ primer appTests/primer_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  primer_appTests.swift
+//  primer appTests
+//
+//  Created by José Jesús Moreno Ríos on 06/04/25.
+//
+
+import Testing
+@testable import primer_app
+
+struct primer_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
